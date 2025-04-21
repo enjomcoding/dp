75ce03cd0cdcbf9b641bbad86781bce37e89ca21
Jomarie Tijada <jstijada@unp.edu.ph>
2025-04-21 23:22:07 +0800

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,7 +1,12 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//todo: <<Matrix Sizes>>
+/*
+Matrix A: 2 x 1
+Matrix B: 1 x 2
+Matrix C: 1 x 2 should be 2 x 2 
+Matrix D: 2 x 2
+*/
 public class Lab2 {
 
     public static void main(String[] args) {
@@ -11,9 +16,10 @@ public class Lab2 {
         int num = input.nextInt();
 
         ArrayList<Integer> dim = new ArrayList<>();
-        int[][] cols_rows = new int[num + 1][2];
+        int[][] cols_rows = new int[num][2];
         ArrayList<int[][]> matrices = new ArrayList<>();
 
+        // Input Matrix A and B
         for (int i = 0; i < 2; i++) {
             char mtrx_name = (char) ('A' + i);
             System.out.println("\n<Matrix " + mtrx_name + ">");
@@ -22,33 +28,38 @@ public class Lab2 {
             System.out.print("-Input Row: ");
             int row = input.nextInt();
 
-            cols_rows[i][0] = col;
-            cols_rows[i][1] = row;
+            cols_rows[i][0] = col;  // col
+            cols_rows[i][1] = row;  // row
 
+            // Build dim[] based on COLUMN then ROW input
             if (i == 0) {
-                dim.add(col);
-                dim.add(row);
+                dim.add(col);  // A's col
+                dim.add(row);  // A's row = B's col
+            }else{
+                dim.add(row);  // B's row
             }
 
-            int[][] matrix = new int[col][row];//changed
+            // Create matrix [row][col] even though user inputs col first
+            int[][] matrix = new int[col][row];
             System.out.println("-Enter Elements:");
             for (int c = 0; c < col; c++) {
                 for (int r = 0; r < row; r++) {
                     System.out.print(mtrx_name + "[" + c + "][" + r + "]: ");
-                    matrix[c][r] = input.nextInt();
+                    matrix[c][r] = input.nextInt();  // Row first, Column second
                 }
             }
             matrices.add(matrix);
         }
-        System.out.println(dim.size());
+
+        // Compute remaining dimensions (C to N)
         dim = Matrix_Chain_Mult.get_preDim(dim, num);
-        //System.out.println(">>>>>>>????");
-        System.out.println(dim.size());
-        int ind = 1;
+        System.out.println("ewfrwt" + dim.size());
+
+        int index = 2;
         for (int i = 2; i < num; i++) {
-            cols_rows[i][0] = dim.get(ind);     // col of matrix i
-            cols_rows[i][1] = dim.get(ind + 1); // row of matrix i
-            ind += 2;
+            cols_rows[i][0] = dim.get(index);     // col
+            cols_rows[i][1] = dim.get(index + 1); // row
+            index++;
         }
 
         System.out.println("\n<<Matrix Sizes>>");
@@ -58,29 +69,29 @@ public class Lab2 {
         }
 
         System.out.println("\n<<Matrices>>");
+        
         for (int i = 0; i < matrices.size(); i++) {
             char matrixName = (char) ('A' + i);
             System.out.println("Matrix " + matrixName + ":");
 
-            int[][] matrix = matrices.get(i);//chnaged
+            int[][] matrix = matrices.get(i);
             for (int c = 0; c < matrix.length; c++) {
-                for (int r= 0; r < matrix[c].length; r++) {
+                for (int r = 0; r < matrix[0].length; r++) {
                     System.out.print(matrix[c][r] + " ");
                 }
                 System.out.println();
             }
             System.out.println();
         }
-        System.out.println("<<Matrix Multiplication>>");
-        Matrix_Chain_Mult.get_pro(matrices);
 
-        // Optional: print dim[]
+        System.out.println("<<Matrix Multiplication>>");
+        Matrix_Chain_Mult.get_pro(dim);
+        
+        // Print dim[] array
         System.out.print("dim[] = ");
         for (int d : dim) {
             System.out.print(d + " ");
         }
         System.out.println();
     }
-
-    
 }
